de2133c6f0bd12f2e785b4fe9224125ff7eb09ad
Kaique-Braatz-Silva <132665929+Kaique-Braatz-Silva@users.noreply.github.com>
2023-05-19 16:14:52 +0000

    implementação da estrutura

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,19 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  calendario= {
+
+    mes: 'Maio',
+    ano: 2023,
+    foto: 'https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    dias_da_semana:['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB', ],
+    primeira_semana:[30, 1, 2, 3, 4, 5, 6],
+    segunda_semana:[8, 9, 10, 11, 12, 13, ],
+    terceira_semana:[14, 15, 16, 17, 18, 19, 20, ],
+    quarta_semana:[21, 22, 23, 24, 25, 26, 27],
+    quinta_semana:[28, 29, 30, 31],
+  }
 }
