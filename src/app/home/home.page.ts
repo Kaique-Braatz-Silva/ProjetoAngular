3c7d2fe7a88f318a908bb41d64c7bd48ba2a132f
Kaique-Braatz-Silva <132665929+Kaique-Braatz-Silva@users.noreply.github.com>
2023-06-16 16:16:44 +0000

    implementacao da calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,5 +9,71 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  inputValue: string = ''; // Variável para armazenar o valor do input
+  resultado: any;
+  valor1: any;
+  valor2: any;
+  operador: any;
+
+  exibir(valor: number) {
+    if (this.operador) {
+      
+      this.valor2 = valor;
+    } else {
+    
+      this.valor1 = valor;
+    }
+    this.inputValue += valor; 
+  }
+
+
+
+  operacao(operador: any) {
+    this.operador = operador; 
+  }
+
+  calcular() {
+  
+    switch (this.operador) {
+      case '+':
+        this.resultado = this.valor1 + this.valor2;
+        break;
+      case '-':
+        this.resultado = this.valor1 - this.valor2;
+        break;
+      case '*':
+        this.resultado = this.valor1 * this.valor2;
+        break;
+      case '/':
+        this.resultado = this.valor1 / this.valor2;
+        break;
+      case '%':
+        this.resultado = this.valor1 % this.valor2;
+        break;
+      default:
+        this.resultado = null;
+    }
+
+  
+    this.valor1 = null;
+    this.valor2 = null;
+    this.operador = null;
+
+
+    this.inputValue = this.resultado;
+  }
+
+  limpar() {
+    this.inputValue = ''; 
+    this.valor1 = null; 
+    this.valor2 = null; 
+    this.operador = null; 
+    this.resultado = null;
+  }
+
+  apagar() {
+    this.inputValue = this.inputValue.slice(0, -1); 
+  }
+  
+
 }
